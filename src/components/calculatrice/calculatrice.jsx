fd6b2db94edc1d698a28ac2e2ecd926845a9964f
Aurélien <aurelien.strimelle@bstorm.be>
2022-02-01 16:10:26 +0100

    End of demo

--- src/components/calculatrice/calculatrice.jsx
+++ src/components/calculatrice/calculatrice.jsx
@@ -16,29 +16,64 @@ const Calculatrice = () => {
     const [result, setResult] = useState('');
 
     const handleNumberInput = (e, setStateValue) => {
-        const regexNumber = /^[0-9]+([.,][0-9]*)?$/
+        const regexNumber = /^[0-9]+([.,][0-9]*)?$/;
         const value = e.target.value;
 
         console.log(value);
 
-        if(value === '' || regexNumber.test(value)) {
+        if (value === '' || regexNumber.test(value)) {
             setStateValue(value.replace(',', '.'));
         }
-    }
+    };
+
+    const handleCalculEuh = (e) => {
+        e.preventDefault();
+
+        const val1 = parseFloat(nb1);
+        const val2 = parseFloat(nb2);
+
+        if(isNaN(val1) || isNaN(val2)) {
+            setResult('Fool of a Took!');
+            return;
+        }
+
+        switch (operation) {
+            case operationEnum.add:
+                setResult(val1 + val2);
+                break;
+            case operationEnum.sub:
+                setResult(val1 - val2);
+                break;
+            case operationEnum.div:
+                if(val2 === 0) {
+                    setResult('Division par Zero :o');
+                }
+                else {
+                    setResult(val1 / val2);
+                }
+                break;
+            case operationEnum.mult:
+                setResult(val1 * val2);
+                break;
+            default:
+                setResult('Selectionnez une opération');
+                break;
+        }
+    };
 
     return (<>
         <h2>Calculatrice</h2>
-        <form>
+        <form onSubmit={handleCalculEuh}>
             <div>
                 <label htmlFor='nb1'>Nb 1 : </label>
-                <input type='text' 
+                <input type='text'
                     onChange={(e) => handleNumberInput(e, setNb1)}
                     value={nb1} />
             </div>
             <div>
                 <label htmlFor='op'>Operation : </label>
-                <select id='op' 
-                    onChange={(e) => setOperation(e.target.value)} 
+                <select id='op'
+                    onChange={(e) => setOperation(e.target.value)}
                     value={operation}>
                     <option value='choice'>-- Selection une option --</option>
                     <option value={operationEnum.add}>+</option>
@@ -51,14 +86,14 @@ const Calculatrice = () => {
                 <label htmlFor='nb2'>Nb 2 : </label>
                 <input type='text'
                     onChange={(e) => handleNumberInput(e, setNb2)}
-                    value={nb2}/>
+                    value={nb2} />
             </div>
             <div>
-                <input type='submit' value='Calculer'/>
-                <input type='text' value={result} readOnly/>
+                <input type='submit' value='Calculer' />
+                <input type='text' value={result} readOnly />
             </div>
         </form>
-    </>)
-}
+    </>);
+};
 
 export default Calculatrice;
